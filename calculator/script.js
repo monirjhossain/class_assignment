cf2eefd0c83248b7be6271711a2696c57414de46
Md. Monir Hossain <59936758+monirjhossain@users.noreply.github.com>
2024-04-15 20:29:14 +0600

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,36 +1,3 @@
-/*
-    function calculate() {
-      var num1 = parseFloat(document.getElementById('num1').value);
-      var num2 = parseFloat(document.getElementById('num2').value);
-      var operation = document.getElementById('operation').value;
-      var result;
-
-      switch(operation) {
-        case 'add':
-          result = num1 + num2;
-          break;
-        case 'subtract':
-          result = num1 - num2;
-          break;
-        case 'multiply':
-          result = num1 * num2;
-          break;
-        case 'divide':
-          if(num2 !== 0) {
-            result = num1 / num2;
-          } else {
-            result = 'Cannot divide by zero';
-          }
-          break;
-        default:
-          result = 'Invalid operation';
-          break;
-      }
-
-      document.getElementById('result').style.display = 'block';
-      document.getElementById('result').innerHTML = 'Result: ' + result;
-    }
-  */
 
 function logic() {
   const number1 = parseFloat(document.getElementById('number1').value);
